bd10667ce55eced5cc08c9a20a16b48c994ce977
Wesley Farias <wesleysousukee@gmail.com>
2022-02-20 17:12:53 -0300

    adiciona views, urls e templates recipes -category

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -4,4 +4,6 @@ from .import views
 app_name = 'recipes'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('receitas/detalhe/<int:id>/', views.recipe, name='recipe'),
+    path('receita/categoria/<int:id>/', views.category, name='category'),
 ]
--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Recipe, Category
 
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True)
     return render(request, 'pages/index.html', {'recipes': recipes})
+
+
+def category(request, id):
+    category = get_list_or_404(Recipe, is_published=True, category__id=id)
+    return render(request, 'pages/category.html', {'category': category})
+
+
+def recipe(request, id):
+    recipe = get_object_or_404(Recipe, id=id, is_published=True)
+    return render(request, 'pages/recipe.html', {'recipe': recipe})
